9a0eaf63d844fe782e5b9375491b13a96862084f
NeSoSiSka <78197152+NeSoSiSka@users.noreply.github.com>
2021-03-11 21:18:18 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length == 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) !== true || array.length == 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (Array.isArray(array) !== true || array.length == 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    return avg= sum / array.length;
+  }
 }
